b294705de058623a2c962f42a84613e08c558f6a
Pawan Kumar <pkumrbh@amazon.com>
2021-12-14 18:50:07 +0530

    loops

---
+++ Loops/print_1_to_n.py
@@ -0,0 +1,19 @@
+n = int(input("Enter any number : "))
+
+# While loop solu.
+
+
+'''
+num = 1
+
+while(num <= n):
+    print(num)
+    num += 1
+
+'''
+
+
+# For loop solu
+
+for num in range(1, n+1):
+    print(num)
---
+++ Loops/print_n_to_1.py
@@ -0,0 +1,18 @@
+n = int(input("Enter any number : "))
+
+# While loop solu.
+
+'''
+num = n
+
+while(num > 0):
+    print(num)
+    num -= 1
+
+'''
+
+
+# For loop solu
+
+for num in range(n, 0, -1):
+    print(num)
