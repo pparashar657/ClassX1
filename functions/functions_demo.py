fbea02310f2e421f625ed472ea32271f7825c583
Pawan Kumar <pkumrbh@amazon.com>
2021-12-27 18:00:05 +0530

    functionDemo

---
+++ functions/functions_demo.py
@@ -0,0 +1,25 @@
+def say_hello():
+    print("Hello World")
+
+
+def say_hello_name(name):
+    print("Hello", name)
+
+# say_hello()
+
+
+'''
+say_hello_name("Pawan")
+say_hello_name("Kartik")
+say_hello_name("Adi")
+
+'''
+
+
+def addTwo(num1, num2):
+    print(num1, num2)
+    ans = num1 + num2
+    print(ans)
+
+
+addTwo(10, 15)
