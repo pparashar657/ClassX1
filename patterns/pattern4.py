cacec2f975d3178e1015ffd7f2dce849bb99a21b
Pawan Kumar <pkumrbh@amazon.com>
2022-01-19 18:01:26 +0530

    pattern que

--- patterns/pattern4.py
+++ patterns/pattern4.py
@@ -8,6 +8,19 @@ def pattern4(n):
         print()
 
 
+def pattern4_v2(n):
+    for row in range(1, n + 1):
+        # Print spaces
+        for col in range(n-row):
+            print(" ", end=' ')
+
+        # Print Stars
+        for col in range(row):
+            print("*", end=" ")
+
+        print()
+
+
 n = int(input("Enter any number : "))
 
-pattern4(n)
+pattern4_v2(n)
