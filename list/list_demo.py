e53f06b62d44b382776cf372b314cb4b4e7b57ba
Pawan Kumar <pkumrbh@amazon.com>
2022-01-25 18:04:09 +0530

    list intro

--- list/list_demo.py
+++ list/list_demo.py
@@ -1,13 +1,60 @@
 
-my_list = [18.34, 2, "pawan", 4, True]
-print(my_list[0])
-print(my_list[3])
+# [1,2,3,4,5]
+
+'''
+print(12)
+
+print([1, 2, 5, 4, 6, 7])
+
+a = 12
+
+print("a = ", a)
+
+my_list = [1, 5, 2, 6]
+
+print("My list = ", my_list)
+
 print(my_list[1])
 
-a = my_list[2]
-b = my_list[0] * my_list[3]
-print(a + " Kumar")
-print(b)
+a = my_list[3] * 2 + my_list[0]
 
-print(type(my_list[2]))
-print(type(my_list))
+print(a)
+
+'''
+
+my_list = [1, 5, 2, 6]
+
+print(my_list)
+my_list[0] = 10
+print(my_list)
+
+my_list[3] = my_list[0] + my_list[2]
+
+print(my_list)
+
+my_list = [7]
+
+print(my_list)
+
+print(type(10))
+print(type([1, 5, 2]))
+
+a = 10
+
+print(type(a))
+
+b = [1, 2]
+
+print(type(b))
+
+print(type(b[0]))
+
+kartik = ['Adi', 10, True, 10.57]
+
+print(type(kartik))
+
+print(type(kartik[0]))
+
+kartik[0] = 19
+
+print(kartik)
