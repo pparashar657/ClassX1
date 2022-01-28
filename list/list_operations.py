d471a1403cb42866f65c7219916f019ec067ce2b
Pawan Kumar <pkumrbh@amazon.com>
2022-01-28 18:07:34 +0530

    list slicing and traversal

--- list/list_operations.py
+++ list/list_operations.py
@@ -22,7 +22,6 @@
 # Operation 2: Repetition
 
 
-from re import A
 '''
 a = 2
 
@@ -45,7 +44,7 @@ a * 3 == a + a + a
 
 # Operation 3: Slicing
 
-list = [1, 2, 9, 8, 4, 7, 6]
+# list = [1, 2, 9, 8, 4, 7, 6]
 
 # print(list[2:4])
 # print(list[:])
@@ -56,4 +55,40 @@ list = [1, 2, 9, 8, 4, 7, 6]
 # print(list[2:] + list[2:6] + list[:6])
 
 
-print(list[1:-3])
+# print(list[1:-3])
+
+# list = [1, 5, 7, 8, 4, 3]
+
+# print(list[::])
+# print(list[::2])
+# print(list[1:5:2])
+
+# print(list[:4:3])
+
+# print(list[::-1])
+
+# print(list[2:5:-1])
+# print(list[5:2:-1])
+
+# print(list[5::-1])
+
+# print(list[:2:-1])
+
+
+# list = [1, 5, 7, 8, 4, 3]
+
+# print(list[1:-1:1])
+
+# print(list[-1:0:-1])
+
+# print(list[-5:-1:-1])
+
+# Operation 4: Includes
+
+
+list = ["Apple", "orange", "Kiwi"]
+
+if('Orange' not in list):
+    print("My fruits has orange")
+else:
+    print("Fruits does not contain orange")
---
+++ list/list_traversal.py
@@ -0,0 +1,5 @@
+
+list = [2, 8, 1, 5, 7, 3, 4]
+
+for kartik in list:
+    print(kartik, end=" ")
