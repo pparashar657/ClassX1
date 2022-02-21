269b09663e99b079a099e57cf49c8a0a0f160cd9
Pawan Kumar <pkumrbh@amazon.com>
2022-02-21 18:00:04 +0530

    String functions

--- Strings/string_func.py
+++ Strings/string_func.py
@@ -1,57 +1,138 @@
 
 # len
 
+from curses.ascii import isalnum
+
+
 str = "Pawan is a good boy"
 
-print(len(str))
+# print(len(str))
 
-print(max(str))
+# print(max(str))
 
-print(min(str))
+# print(min(str))
 
-ch_arr = sorted(str)
+# ch_arr = sorted(str)
 
-ch_arr = ['p', 'a', 'w', 'a', 'n']
+# ch_arr = ['p', 'a', 'w', 'a', 'n']
 
-ans = ""
+# ans = ""
 
-for ch in ch_arr:
-    ans = ans + ch
+# for ch in ch_arr:
+#     ans = ans + ch
 
-print(ans)
+# print(ans)
 
 
-#  string class functions.
+# #  string class functions.
 
-str = "Panwan is a gooD boy"
+# str = "Panwan is a gooD boy"
 
-# Title fucntion
+# # Title fucntion
 
-print(str.title())
+# print(str.title())
+
+# print(str)
+
+# # Lower
+
+# print(str.lower())
+
+# # upper
+# print(str.upper())
+
+# # count
+
+# print(str.count('Pan'))
+
+# print(str.count('a', 4, 11))
+
+# # find
+
+# print(str.find('is'))
+
+# print(str.find('is', 8, 10))
+
+# #  index
+
+# print(str.index('is'))
+
+# print(str.index('is', 8, 10))
+
+# endsWith
+
+# print(str)
+
+# print(str.endswith('a good'))
+
+# startsWith
+
+# print(str.startswith('pawan'))
+
+
+# a = '10'
+
+# print(type(a[0]))
+
+
+# str = "Pawan"
+
+# # str = None
+
+# print(str.isalnum())
+
+# print(str.isalpha())
+
+# str = "Pawan123"
+# print(str.isalnum())
+# print(str.isalpha())
+
+# str = "10"
+# print(str.isnumeric())
+
+# str = "paWan "
+
+# print(str.islower())
+
+# str = "ADI"
+
+# print(str.isupper())
+
+# str = "    "
+
+# print(str.isspace())
+
+str = "      Hello World      "
+
+# Strip
+print(str.strip())
+print(str.lstrip()+"Ok")
+print(str.rstrip()+"ok")
+
+# replace
+
+str = "Pawan is a good boy and a good dancer."
+
+print(str.replace("good", "very good"))
+print(str.replace("o", "x", 2))
 
 print(str)
 
-# Lower
+# Join
 
-print(str.lower())
+list = ["Pawan", "is", "a", "good", "boy"]
 
-# upper
-print(str.upper())
+str = ' '.join(list)
 
-# count
+print(str)
 
-print(str.count('Pan'))
+# split
+words = str.split('is')
+print(words)
+print(type(words))
 
-print(str.count('a', 4, 11))
+# partition
 
-# find
-
-print(str.find('is'))
-
-print(str.find('is', 8, 10))
-
-#  index
-
-print(str.index('is'))
-
-print(str.index('is', 8, 10))
+part = str.partition(' ')
+print(part)
+print(type(part))
